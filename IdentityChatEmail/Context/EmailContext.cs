7947f64b62889a63d938b436c11b935a41c0d6f3
Elif Alptekin <ealptekina@gmail.com>
2025-04-29 22:13:26 +0300

    Add project files.

---
+++ IdentityChatEmail/Context/EmailContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityChatEmail.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityChatEmail.Context
+{
+    public class EmailContext:IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-IMEIVGC\\SQLEXPRESS;initial Catalog=EmailChat;integrated security=true;trust server certificate=true");
+        }
+    }
+}
---
+++ IdentityChatEmail/Controllers/RegisterController.cs
@@ -0,0 +1,58 @@
+﻿using IdentityChatEmail.Entities;
+using IdentityChatEmail.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityChatEmail.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // Identity kullanıcı yöneticisi bağımlılığı (AppUser tipi ile)
+        private readonly UserManager<AppUser> _userManager;
+
+        // Constructor (bağımlılık enjeksiyonu ile UserManager nesnesi alınır)
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // Kayıt sayfası görüntülendiğinde çalışacak GET metodu
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View(); // Boş formu döndürür
+        }
+
+        // Kayıt formu gönderildiğinde çalışacak POST metodu
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(RegisterViewModel model)
+        {
+            // ViewModel'den gelen verilerle yeni kullanıcı nesnesi oluşturuluyor
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,               // Adı
+                Surname = model.Surname,         // Soyadı
+                Email = model.Email,             // E-posta
+                UserName = model.UserName        // Kullanıcı adı
+            };
+
+            // Identity sistemi üzerinden kullanıcı oluşturma işlemi yapılır.Password burada hashli gönderilir.
+            var result = await _userManager.CreateAsync(appUser, model.Password);
+
+            if (result.Succeeded)
+            {
+                // Kayıt başarılıysa, login sayfasına yönlendirilir
+                return RedirectToAction("UserLogin", "Login");
+            }
+            else
+            {
+                // Kayıt başarısızsa, hata mesajları eklenir ve form tekrar gösterilir
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+                return View();
+            }
+        }
+    }
+}
